025c44ecf1e1dc92f865579c1d0c9c6269daf621
devthuan <devthuan24@gmail.com>
2026-03-30 23:55:16 +0700

    Remove unused Async import from AuditLogService

--- src/main/java/com/smartvoucher/service/AuditLogService.java
+++ src/main/java/com/smartvoucher/service/AuditLogService.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
